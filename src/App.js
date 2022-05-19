a1c3d66d6993640086e19dad9c50d607d159ea61
Mikael <dev.kov.mtl@gmail.com>
2022-05-19 16:47:39 -0400

    Compute the total

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Navbar from './components/Navbar';
 import CartContainer from './components/CartContainer';
+import { calculateTotals } from './features/cart/cartSlice';
+
 function App() {
+  const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
   return (
     <main>
       <Navbar />
