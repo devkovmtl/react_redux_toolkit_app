7117634b17c63850deadad566dc4d8647eff6f28
Mikael <dev.kov.mtl@gmail.com>
2022-05-19 20:58:16 -0400

    Clear cart and close modal

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,6 +1,7 @@
 import { useSelector, useDispatch } from 'react-redux';
 import CartItem from './CartItem';
-import { clearCart } from '../features/cart/cartSlice';
+// import { clearCart } from '../features/cart/cartSlice';
+import { openModal } from '../features/modal/modalSlice';
 
 const CartContainer = () => {
   const { cartItems, total, amount } = useSelector((state) => state.cart);
@@ -38,7 +39,7 @@ const CartContainer = () => {
             total <span>${total.toFixed(2)}</span>
           </h4>
         </div>
-        <button className='btn clear-btn' onClick={() => dispatch(clearCart())}>
+        <button className='btn clear-btn' onClick={() => dispatch(openModal())}>
           clear cart
         </button>
       </footer>
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,13 +1,30 @@
+import { useDispatch } from 'react-redux';
+import { clearCart } from '../features/cart/cartSlice';
+import { closeModal } from '../features/modal/modalSlice';
+
 const Modal = () => {
+  const dispatch = useDispatch();
+
   return (
     <aside className='modal-container'>
       <div className='modal'>
         <h4>Remove all items from your shopping cart?</h4>
         <div className='brn-container'>
-          <button className='btn confirm-btn' type='button'>
+          <button
+            className='btn confirm-btn'
+            type='button'
+            onClick={() => {
+              dispatch(clearCart());
+              dispatch(closeModal());
+            }}
+          >
             confirm
           </button>
-          <button className='btn clear-btn' type='button'>
+          <button
+            className='btn clear-btn'
+            type='button'
+            onClick={() => dispatch(closeModal())}
+          >
             cancel
           </button>
         </div>
